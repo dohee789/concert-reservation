38b09496da31867640c5d991057adb978f861476
dohee789 <dohee@doheeui-MacBookPro.local>
2025-04-03 20:24:45 +0900

    :package: 패키지 구조 변경

--- src/main/java/kr/hhplus/concert/config/SwaggerConfig.java
+++ src/main/java/kr/hhplus/concert/config/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server.config.swagger;
+package kr.hhplus.concert.config;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
--- src/test/java/kr/hhplus/concert/TestcontainersConfiguration.java
+++ src/test/java/kr/hhplus/concert/TestcontainersConfiguration.java
@@ -1,4 +1,4 @@
-package kr.hhplus.be.server;
+package kr.hhplus.concert;
 
 import jakarta.annotation.PreDestroy;
 import org.springframework.boot.test.context.TestConfiguration;
