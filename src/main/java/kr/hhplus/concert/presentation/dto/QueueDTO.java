b19fcd1928580b6141aa9cfdbaa5e591a1f89119
dohee789 <dohee@doheeui-MacBookPro.local>
2025-04-04 05:49:40 +0900

    :white_check_mark: e2e 테스트코드 추가

--- src/main/java/kr/hhplus/concert/presentation/dto/QueueDTO.java
+++ src/main/java/kr/hhplus/concert/presentation/dto/QueueDTO.java
@@ -1,15 +1,13 @@
 package kr.hhplus.concert.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 
 public class QueueDTO {
     @Getter
+    @Setter
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
@@ -21,9 +19,9 @@ public class QueueDTO {
     @Builder
     public static class Response {
         private String token;
-        private enum type {
+        public enum type {
             ACTIVATE, WAITING
-        };
+        }
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
         private LocalDateTime enteredAt;
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
--- src/main/java/kr/hhplus/concert/presentation/dto/ReservationDTO.java
+++ src/main/java/kr/hhplus/concert/presentation/dto/ReservationDTO.java
@@ -1,16 +1,14 @@
 package kr.hhplus.concert.presentation.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 public class ReservationDTO {
     @Getter
+    @Setter
     @Builder
     @NoArgsConstructor
     @AllArgsConstructor
