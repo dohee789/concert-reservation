b920e83cb70e46f3d8c2f9b5382299bf48eaf053
dohee789 <dohee@doheeui-MacBookPro.local>
2025-04-17 20:33:55 +0900

    :sparkles: 토근, 대기열 관련 DB 설계

---
+++ src/main/java/kr/hhplus/concert/infrastructure/entity/QueueEntity.java
@@ -0,0 +1,60 @@
+package kr.hhplus.concert.infrastructure.entity;
+
+import jakarta.persistence.*;
+import kr.hhplus.concert.domain.model.Queue;
+import kr.hhplus.concert.domain.model.enums.QueueStatus;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "queue", indexes = {
+        @Index(name = "idx_token", columnList = "token"),
+        @Index(name = "idx_user_id", columnList = "user_id")
+})
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+
+public class QueueEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
+    private UserEntity user;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private QueueStatus queueStatus;
+
+    private LocalDateTime expiredAt;
+    private LocalDateTime enteredAt;
+
+    public static QueueEntity from(Queue queue) {
+        return QueueEntity.builder()
+                .id(queue.getId())
+                .user(UserEntity.builder().id(queue.getUserId()).build())
+                .token(queue.getToken())
+                .queueStatus(queue.getQueueStatus())
+                .expiredAt(queue.getExpiredAt())
+                .enteredAt(queue.getEnteredAt())
+                .build();
+    }
+
+    public Queue of() {
+        return Queue.builder()
+                .id(this.id)
+                .userId(this.user.getId())
+                .token(this.token)
+                .queueStatus(this.queueStatus)
+                .expiredAt(this.expiredAt)
+                .enteredAt(this.enteredAt)
+                .build();
+    }
+}
---
+++ src/main/java/kr/hhplus/concert/infrastructure/repository/QueueRepositoryImpl.java
@@ -0,0 +1,55 @@
+package kr.hhplus.concert.infrastructure.repository;
+
+import kr.hhplus.concert.domain.model.Queue;
+import kr.hhplus.concert.domain.model.enums.QueueStatus;
+import kr.hhplus.concert.domain.repository.QueueRepository;
+import kr.hhplus.concert.infrastructure.entity.QueueEntity;
+import org.springframework.transaction.annotation.Transactional;
+import lombok.RequiredArgsConstructor;
+import java.util.*;
+
+@RequiredArgsConstructor
+public class QueueRepositoryImpl implements QueueRepository {
+
+    private final QueueJpaRepository queueJpaRepository;
+
+    @Override
+    public Optional<Queue> findByUserId(Long userId) {
+        return queueJpaRepository.findById(userId)
+                .map(QueueEntity::of);
+    }
+
+    @Override
+    @Transactional
+    public Queue save(Queue queue) {
+        QueueEntity queueEntity = queueJpaRepository.save(QueueEntity.from(queue));
+        return queueEntity.of();
+    }
+
+    @Override
+    public Long countActiveQueues() {
+        return queueJpaRepository.findAll().stream()
+                .filter(q -> q.getQueueStatus() == QueueStatus.ACTIVE)
+                .count();
+    }
+
+    @Override
+    public Integer countAheadOf(Long userId) {
+        QueueEntity enqueued = queueJpaRepository.findById(userId)
+                .filter(q -> q.getQueueStatus() == QueueStatus.ACTIVE)
+                .orElseThrow(() -> new NoSuchElementException("유효한 ACTIVE 상태의 유저가 없습니다."));
+
+        return (int) queueJpaRepository.findAll().stream()
+                .filter(q -> q.getQueueStatus() == QueueStatus.ACTIVE)
+                .filter(q -> q.getEnteredAt().isBefore(enqueued.getEnteredAt()))
+                .count();
+    }
+
+    @Override
+    public List<Queue> findAll() {
+        return queueJpaRepository.findAll().stream()
+                .map(QueueEntity::of)
+                .toList();
+    }
+}
+
