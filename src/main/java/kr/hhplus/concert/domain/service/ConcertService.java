96863b2b9d29763da27e4d5b4092b5e18ba8219f
dohee789 <dohee@doheeui-MacBookPro.local>
2025-05-08 01:19:04 +0900

    :zap: 콘서트, 콘서트 스케줄 조회 캐싱 처리

--- src/main/java/kr/hhplus/concert/domain/service/ConcertService.java
+++ src/main/java/kr/hhplus/concert/domain/service/ConcertService.java
@@ -3,7 +3,9 @@ package kr.hhplus.concert.domain.service;
 import kr.hhplus.concert.domain.model.Concert;
 import kr.hhplus.concert.domain.repository.ConcertRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -13,6 +15,8 @@ import java.util.NoSuchElementException;
 public class ConcertService {
     private final ConcertRepository concertRepository;
 
+    @Cacheable(value = "concert", key = "#id", unless = "#result == null")
+    @Transactional(readOnly = true)
     public Concert findConcertById(Long id) {
         return concertRepository.findById(id)
                 .orElseThrow(() -> new NoSuchElementException("콘서트 정보를 찾을 수 없습니다."));
