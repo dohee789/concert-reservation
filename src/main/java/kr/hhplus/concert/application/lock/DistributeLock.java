358cf4dc131be35bc825d536b235cd357338acfc
dohee789 <dohee@doheeui-MacBookPro.local>
2025-05-12 23:57:41 +0900

    :zap: Feedback 반영

--- src/main/java/kr/hhplus/concert/application/lock/DistributeLock.java
+++ src/main/java/kr/hhplus/concert/application/lock/DistributeLock.java
@@ -1,12 +1,14 @@
 package kr.hhplus.concert.application.lock;
 
 import java.lang.annotation.*;
+import java.util.concurrent.TimeUnit;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 public @interface DistributeLock {
     String key();         // SpEL로 동적 키 만들기
+    TimeUnit timeUnit() default TimeUnit.SECONDS;
     long waitTime() default 5L;
     long leaseTime() default 5L;
 }
