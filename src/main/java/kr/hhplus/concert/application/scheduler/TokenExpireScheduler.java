d8b6bec695a8c85c0371c17f2a6a2abb5e2bd478
dohee789 <dohee@doheeui-MacBookPro.local>
2025-05-16 03:42:10 +0900

    :zap: 토큰+대기열 로직 레디스 적용

--- src/main/java/kr/hhplus/concert/application/scheduler/TokenExpireScheduler.java
+++ src/main/java/kr/hhplus/concert/application/scheduler/TokenExpireScheduler.java
@@ -1,24 +1,30 @@
 package kr.hhplus.concert.application.scheduler;
-
-import kr.hhplus.concert.domain.model.Queue;
-import kr.hhplus.concert.domain.repository.QueueRepository;
+import kr.hhplus.concert.infrastructure.repository.QueueRedisRepository;
 import org.springframework.scheduling.annotation.Scheduled;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
+import java.util.Set;
 
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class TokenExpireScheduler {
 
-    private QueueRepository queueRepository;
+    private final QueueRedisRepository queueRedisRepository;
 
-    @Scheduled(fixedDelay = 5000*60)
+    @Scheduled(fixedDelay = 60_000*5) // 5분마다 실행
     public void expireInactiveTokens() {
-        List<Queue> queues = queueRepository.findAll();
+        Set<String> userIds = queueRedisRepository.getAllUserIds();
 
-        for (Queue queue : queues) {
-            if (queue.isExpired()) {
-                queue.expireToken();
-                queueRepository.save(queue);
+        for (String userId : userIds) {
+            boolean exists = queueRedisRepository.existsTokenKey(userId);
+            if (!exists) {
+                log.info("만료된 유저 [{}]를 큐에서 제거합니다.", userId);
+                queueRedisRepository.remove(Long.valueOf(userId));
             }
         }
     }
 }
+
